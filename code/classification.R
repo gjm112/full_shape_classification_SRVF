d02ced6dab16008774350987d627c8aabdf345e6
Gregoery J. Matthews <gregorymatthews@Gregoerys-MacBook-Pro.local>
2021-11-22 15:51:32 -0600

    data and code.

--- code/classification.R
+++ code/classification.R
@@ -6,6 +6,7 @@
 #Load packages
 library(randomForest)
 library(tidyverse)
+library(tune)
 
 
 toothtype <- "LM1"
@@ -13,7 +14,7 @@ i <- 1
 
 #Read in the data
 results <- list()
-
+for (i in 1:5){print(i)
 X_train <- read.csv(paste0("/Users/gregorymatthews/Dropbox/full_shape_classification_SRVF/data/",toothtype,"/",toothtype,"fold_train",i,".csv"), header = FALSE)
 X_test <- read.csv(paste0("/Users/gregorymatthews/Dropbox/full_shape_classification_SRVF/data/",toothtype,"/",toothtype,"fold_test",i,".csv"), header = FALSE)
 
@@ -23,6 +24,32 @@ y_test <- read.csv(paste0("/Users/gregorymatthews/Dropbox/full_shape_classificat
 y_train <- (as.factor(y_train$V1))
 y_test <- (as.factor(y_test$V1))
 
-a <- randomForest(X_train, y_train)
-results[[i]] <- data.frame(pred_class = predict(a, X_test), real_class = y_test)
+#tune the random forest
+fold <- sample(1:5, nrow(X_train), replace = TRUE)
+acc <- c()
+mtry_vec <- seq(10,100,10)
+for (m in 1:length(mtry_vec)){print(m)
+tune_list <- list()
+for (j in 1:5){print(j)
+tune_forest <- randomForest(X_train[fold != j,], y_train[fold != j], mtry = mtry_vec[m])
+tune_list[[j]] <- data.frame(pred_class = predict(tune_forest, X_train[fold == j,]), real_class = y_train[fold == j])
+}  
+
+tune_preds <- do.call(rbind, tune_list)
+acc[m] <- mean(tune_preds[,1] == tune_preds[,2])
+}
+  
+mtry <- mtry_vec[which.max(acc)]
+rf <- randomForest(X_train, y_train, mtry = mtry)
+
+
+results[[i]] <- data.frame(pred_class = predict(rf, X_test), real_class = y_test)
+
+}
+
+res <- do.call(rbind,results)
+mean(res[,1] == res[,2])
+
+
+(table(results[[1]]))
 
