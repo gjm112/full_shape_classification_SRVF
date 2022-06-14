61b83aacbb5521156bd87c90a999867a8cfc2533
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2022-06-14 11:12:59 -0500

    EFA data.

--- code/R/elliptical_fourier_test.R
+++ code/R/elliptical_fourier_test.R
@@ -27,6 +27,31 @@ for (i in 1:length(teeth_BW_train)){print(i)
 
 names(efList) <- names(ptsList) <- names(teeth_BW_train)
 
+all <- do.call(rbind,ptsList)
+row.names(all) <- names(ptsList)
+
+#Now pull out LM1 
+temp <- all[row.names(all) %in% rs$image[rs$type == "LM1"],]
+
+#Now add PCs
+#Add principle components
+pc<-princomp(temp)$scores
+temp<-cbind(temp,pc[,1:30])
+
+#Now cut it in to folds like Nikki did.
+#Create the folds to match what nikki did.  Then I can just run it through the same code that I alreayd have. 
+folds_ref <-  read.csv("/Users/gregorymatthews/Dropbox/full_shape_classification_SRVF/data/folds/LM1ref_folds.csv")
+
+#Test
+temp[folds_ref$image[folds_ref$folds_tribe == 1],]
+
+#Train
+temp[folds_ref$image[folds_ref$folds_tribe != 1],]
+
+
+
+
+
 
 
 #Make a data frame.  
